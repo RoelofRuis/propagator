cafa1e9644b058a596a31ccfe16a4db156bbf1ff
Roelof Ruis <roelof.ruis@gmail.com>
2025-05-17 23:59:04 +0200

    different approach

--- examples/iq-fit/main.go
+++ examples/iq-fit/main.go
@@ -5,54 +5,69 @@ import (
 	"github.com/RoelofRuis/propagator"
 )
 
-type Cell struct {
-	X, Y  int
-	Shape *propagator.Variable[int]
+type Placement struct {
+	X           int
+	Y           int
+	Orientation int
+}
+
+type Shape struct {
+	Placement *propagator.Variable[Placement]
 }
 
 func main() {
-
 	rows := 3
 	columns := 3
+	// rows := 5
+	// columns := 10
 
-	csp := propagator.NewProblem()
-
-	var grid []*Cell
-
+	var domain []Placement
 	for row := 0; row < rows; row++ {
 		for col := 0; col < columns; col++ {
-			cell := &Cell{
-				X:     col,
-				Y:     row,
-				Shape: propagator.AddVariableFromValues(csp, fmt.Sprintf("%d,%d", col, row), []int{1, 2, 3}),
+			for orientation := 0; orientation < 3; orientation++ {
+				domain = append(domain, Placement{X: col, Y: row, Orientation: orientation})
 			}
-			grid = append(grid, cell)
 		}
 	}
 
+	csp := propagator.NewProblem()
+
+	var shapes []Shape
+	for s := 0; s < 3; s++ {
+		shapes = append(shapes, Shape{
+			Placement: propagator.AddVariableFromValues(csp, fmt.Sprintf("shape %d", s), domain),
+		})
+	}
+
+	csp.AddConstraint(Grid{shapes})
+
 	model := csp.Model()
-	fmt.Printf("model %v\n", model)
 
 	solver := propagator.NewSolver()
 
 	if !solver.Solve(model) {
 		panic("No solution!")
 	}
+
+	for i, shape := range shapes {
+		c := shape.Placement.GetAssignedValue()
+		fmt.Printf("%d: %v\n", i, c)
+	}
 }
 
 type Grid struct {
-	cells []*Cell
+	shapes []Shape
 }
 
 func (q Grid) Scope() []propagator.DomainId {
-	ids := make([]propagator.DomainId, len(q.cells))
-	for _, cell := range q.cells {
-		ids = append(ids, propagator.IdOf(cell.Shape))
+	ids := make([]propagator.DomainId, len(q.shapes))
+	for _, shape := range q.shapes {
+		ids = append(ids, propagator.IdOf(shape.Placement))
 	}
 	return ids
 }
 
 func (q Grid) Propagate(m *propagator.Mutator) {
 	// TODO: implement
-	// probably extend propagate to get the currently set domain ID.
+
 }
